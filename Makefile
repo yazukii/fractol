eb9983c7b984b62794dadc6abe1d333f88f0d431
yazukii <100686337+yazukii@users.noreply.github.com>
2022-11-22 18:06:09 +0100

    testmlx

---
+++ Makefile
@@ -0,0 +1,46 @@
+NAME = fractol
+
+# -framework OpenGL -framework AppKit
+
+LIBFT_PATH = libft
+LIBFT = libft.a
+MLX_PATH = mlx
+MLX = libmlx.dylib
+
+SRC_FILES = main.c
+SRC_DIR = src/
+SRC = ${addprefix ${SRC_DIR}, ${SRC_FILES}}
+OBJ = ${SRC:.c=.o}
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+AR = ar -rcs
+
+%.o: %.c
+	${CC} -c -Imlx -c $< -o $@
+
+all: ${NAME}
+
+libx:
+	@MAKE -sC ${MLX_PATH}
+
+libf:
+	@MAKE -sC ${LIBFT_PATH}
+
+${NAME}: libx libf ${OBJ}
+	${CC} ${LIBFT_PATH}/${LIBFT} ${OBJ} ${MLX_PATH}/${MLX} -o ${NAME}
+
+clean:
+	rm -f ${OBJ}
+	${MAKE} -C ${LIBFT_PATH} clean
+	${MAKE} -C ${MLX_PATH} clean
+
+fclean: clean
+	rm -f ${NAME}
+	${MAKE} -C ${LIBFT_PATH} fclean
+	${MAKE} -C ${MLX_PATH} clean
+	rm -f ${MLX_PATH}/${MLX}
+
+re: fclean all
+
+.PHONY: all clean flcean re
---
+++ src/main.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yidouiss <yidouiss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 17:45:26 by yidouiss          #+#    #+#             */
+/*   Updated: 2022/11/22 17:53:23 by yidouiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../mlx/mlx.h"
+#include "../libft/libft.h"
+#include <unistd.h>
+
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	int		x;
+	int		y;
+	int		trgb;
+	int		i;
+	int sw;
+	t_data	img;
+
+	x = 0;
+	sw = 0;
+	y = 1080 / 2;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "fractol");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_length, &img.endian);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+	return (0);
+}
